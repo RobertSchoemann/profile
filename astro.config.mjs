a7a28baa2716222f1eb9bb3d5521fbcd41adaa7c
Robert Schömann <25429483+RobertSchoemann@users.noreply.github.com>
2024-11-28 18:51:10 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   base: '/profile/',
-  trailingSlash: "never",
+  trailingSlash: "always",
   site: 'https://robertschoemann.github.io',
   integrations: [
     tailwind(),
